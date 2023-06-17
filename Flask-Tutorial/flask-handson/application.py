d4d330668af243f0be2a5cffc1f747e492aff931
anil-k-rajamoni18 <anil.k.rajamoni99@gmail.com>
2023-06-17 20:13:30 +0530

    june 17

--- Flask-Tutorial/flask-handson/application.py
+++ Flask-Tutorial/flask-handson/application.py
@@ -1,10 +1,14 @@
-from flask import Flask , render_template
+from flask import Flask , render_template,request
 
 # create a flask app object 
 
 app = Flask(__name__)
 
 
+credDB = {
+    'username': 'admin',
+    'password': 'june123'
+}
 
 students = [{
     'name': "fenil",
@@ -20,12 +24,26 @@ students = [{
     'isStudent':True,
     'skills' : ['python','java','c']
 },
+{
+    'name': "Ravi",
+    'course': 'java',
+    'level': 2,
+    'isStudent':False,
+    'skills' : ['web','c','cpp']
+},
+{
+    'name': "Shivani",
+    'course': 'python',
+    'level': 2,
+    'isStudent':False,
+    'skills' : ['c']
+},
 ]
 
 # creates routes with decorates.
 @app.route('/')
 def home():
-    return render_template('home.html',data = 'Hello Welcome to my Web-APP')
+    return render_template('home.html',data = 'Hello Welcome to My Web-APP')
 
 @app.route('/students')
 def students_data():
@@ -43,6 +61,36 @@ def get_student(name):
         return {'data': f'student with name {name} not found..'}
  
 
+@app.route("/welcome")
+def welcome():
+    return render_template('welcome.html' , name = 'Kumar')
+
+
+@app.route("/login",methods = ["GET","POST"])
+def login():
+    if request.method == "POST":
+        username = request.form['uname']
+        password = request.form['psw']
+        print(f'{username } , {password}')
+
+        if username == credDB['username'] and password == credDB['password']:
+            return render_template('response.html' , data = f'LOGIN SUCCESS Weclome {username}')
+        else:
+            return render_template('response.html' , data = f'Invalid Creds Please check , try again...')
+
+    return render_template('login.html')
+
+
+@app.route("/contact")
+def contact():
+    address = 'PO BOX NY 5214 Street1'
+    email = 'foodcourt@foodie.com'
+    return render_template('contactus.html' , address=address , email = email)
+
+
+@app.route("/about")
+def about():
+    return render_template('aboutus.html' , data = 'we are rock reciepe food court startup food business...')
 
 if __name__ == '__main__':
     app.run(debug=True)
--- Flask-Tutorial/food-rest-api/app.py
+++ Flask-Tutorial/food-rest-api/app.py
@@ -1,19 +1,44 @@
-from flask import Flask 
+from flask import Flask,request
+from db.MongoUtils import MongoDBConnection
 
+
+mycoll =  MongoDBConnection('rocketRecipeDB','food-items','mongodb://localhost:27017/').create_connection()
 
 app = Flask(__name__)
 
 
+env = {
+    'name' : 'rocket recipe microservice',
+    'version': 'V1.0'
+}
+
+@app.route('/')
+def greet():
+    return f"{env['name']} is UP & RUNNING with version : {env['version']}"
+
 @app.route('/api/food/<int:id>',methods = ['GET'])
-def get_food_item():
+def get_food_item(id):
+    if mycoll is not None:
+        data = mycoll.find()
+        return data
+
+
+@app.route('/api/foods/',methods = ['GET'])
+def get_food_items():
     pass
 
 
 @app.route('/api/food/',methods = ['POST'])
 def save_food_item():
+    if mycoll is not None:
+        data = request.json
+        response = mycoll.insert_one(data)
+        return f' data inserted successfully {response.acknowledged} {response.inserted_id}'
+    return f'Service is not avaiable....'
+
+@app.route('/api/foods/',methods = ['POST'])
+def save_food_items():
     pass
-
-
 
 @app.route('/api/food/<int:id>',methods = ['PUT'])
 def update_food_item():
@@ -27,4 +52,4 @@ def delete_food_item():
 
 if __name__ == '__main__':
     # create db connection..
-    app.run()
+    app.run(debug=True)
